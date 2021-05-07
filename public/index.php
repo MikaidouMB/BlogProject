26000e7b944dafc3bc574734f42cfdf8f56919e1
mikaidou <mikaidoumbo@hotmail.fr>
2021-05-07 14:31:30 +0200

    first commit

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+use Framework\App;
+use GuzzleHttp\Psr7\ServerRequest;
+use function Http\Response\send;
+
+require '../vendor/autoload.php';
+
+$app = new App();
+$response = $app->run(ServerRequest::fromGlobals());
+send($response);
