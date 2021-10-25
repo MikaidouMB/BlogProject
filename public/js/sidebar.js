1c1a9f796fd4c50f6d6ebc6892d7f14375e90fc5
mikaidou <mikaidoumbo@hotmail.fr>
2021-10-25 18:04:11 +0200

    fix-bug-view-footer-admin

---
+++ public/js/sidebar.js
@@ -0,0 +1,15 @@
+    function openNav() {
+    document.getElementById("mySidebar").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "200px";
+        document.getElementById("mainNav").style.marginLeft = "200px";
+        document.getElementById("footer").style.marginLeft = "200px";
+
+    }
+
+    function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+        document.getElementById("mainNav").style.marginLeft= "0";
+        document.getElementById("footer").style.marginLeft= "0";
+
+    }
